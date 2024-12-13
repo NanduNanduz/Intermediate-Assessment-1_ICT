3a04b6a9e059dca41766d8ddd74f03320ca04681
NanduNanduz <nandunsb99@gmail.com>
2024-12-13 23:50:30 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+var image = document.getElementById("imageItem");
+
+function imagereset() {
+  // image.setAttribute("width","28%");
+  image.classList.toggle("imgclass");
+}
+
+function highlight() {
+  listItem.style.color = "Red";
+}
+
+function reset() {
+  listItem.style.color = "Black";
+}
